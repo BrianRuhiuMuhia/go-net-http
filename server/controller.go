e190b083ffcca35a055fc64e1ca46e8d97dac231
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2025-01-31 11:03:20 +0300

    file-upload

--- server/controller.go
+++ server/controller.go
@@ -3,21 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"math/rand"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 )
 
+var current_user = make(map[string]string)
+
 func login(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("something went wrong")
+		return
+	}
 	email := r.FormValue("email")
-	fmt.Println(email)
-	for _, user := range db {
+
+	for _, user := range UserDB {
 		if user.EMAIL == email {
+			current_user["id"] = string(user.ID)
 			http.Redirect(w, r, "/home", http.StatusSeeOther)
 			return
 		}
 	}
+
 	http.Redirect(w, r, "/register", http.StatusSeeOther)
 
 }
@@ -30,8 +41,8 @@ func register(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("password") != r.FormValue("confirm-password") {
 		w.Write([]byte("passwords must match"))
 	}
-	user := User{NAME: r.FormValue("name"), EMAIL: r.FormValue("email"), PASSWORD: r.FormValue("password"), ID: rand.Int()}
-	db = append(db, user)
+	user := User{NAME: r.FormValue("name"), EMAIL: r.FormValue("email"), PASSWORD: r.FormValue("password"), ID: strconv.Itoa(rand.Int())}
+	UserDB = append(UserDB, user)
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
 func loginPage(w http.ResponseWriter, r *http.Request) {
@@ -42,11 +53,57 @@ func registerPage(w http.ResponseWriter, r *http.Request) {
 }
 func homePage(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	data, err := json.Marshal(db)
+	var userImages []Image
+	for _, image := range imageDB {
+		if current_user["id"] == image.USERID {
+			userImages = append(userImages, image)
+		}
+	}
+	data, err := json.Marshal(userImages)
 	if err != nil {
-		fmt.Println("there was an error")
+		fmt.Println("failed conversion")
+		return
 	}
 	w.Write(data)
+}
+func uploadPage(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "../view/upload.html")
+}
+func upload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(32 << 20) // 32 MB limit
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Unable to get file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+	dir := "./test"
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		http.Error(w, "Unable to create directory", http.StatusInternalServerError)
+		return
+	}
+	f, err := os.OpenFile(dir+"/"+handler.Filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	//add to db
+	if err != nil {
+		http.Error(w, "Unable to create file", http.StatusInternalServerError)
+		return
+	}
+	newImage := Image{FILENAME: dir + "/" + handler.Filename, ID: strconv.Itoa(rand.Int()), USERID: current_user["id"]}
+	imageDB = append(imageDB, newImage)
+	defer f.Close()
+	if _, err := io.Copy(f, file); err != nil {
+		http.Error(w, "Unable to save file", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/home", http.StatusCreated)
+}
+func logout(w http.ResponseWriter, r *http.Request) {
+	current_user["id"] = ""
+	http.Redirect(w, r, "/login", http.StatusAccepted)
 }
 func HandleRequsts(w http.ResponseWriter, r *http.Request) {
 	url := strings.Split(r.URL.Path, "/")
@@ -55,12 +112,20 @@ func HandleRequsts(w http.ResponseWriter, r *http.Request) {
 		loginPage(w, r)
 	} else if r.Method == "GET" && route == "register" {
 		registerPage(w, r)
-	} else if r.Method == "GET" && route == "home" {
-		homePage(w, r)
 	} else if r.Method == "POST" && route == "login" {
 		login(w, r)
 	} else if r.Method == "POST" && route == "register" {
 		register(w, r)
+	}
+
+	if r.Method == "GET" && route == "home" && current_user["id"] != "" {
+		homePage(w, r)
+	} else if r.Method == "GET" && route == "upload" && current_user["id"] != "" {
+		uploadPage(w, r)
+	} else if r.Method == "POST" && route == "upload" && current_user["id"] != "" {
+		upload(w, r)
+	} else {
+		http.Redirect(w, r, "/login", http.StatusBadRequest)
 	}
 
 }
--- server/db.go
+++ server/db.go
@@ -1,10 +1,16 @@
 package main
 
 type User struct {
-	ID       int    `json:"id"`
+	ID       string `json:"id"`
 	NAME     string `json:"name"`
 	EMAIL    string `json:"email"`
 	PASSWORD string `json:"password"`
 }
+type Image struct {
+	ID       string `json:"id"`
+	FILENAME string `json:"filename"`
+	USERID   string `json:"userid"`
+}
 
-var db []User
+var UserDB []User
+var imageDB []Image
